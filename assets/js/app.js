ae0327ef960ce7101ac28e94c378818fc58b6cb4
Alhajideen <salifunurudeen834@gmail.com>
2022-09-13 16:57:08 -0600

    add functionality for the mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const dropdown = document.querySelector('.mobile-dropdown');
+const closeX = document.querySelector('.close-x');
+const myBtns = [hamburger, closeX];
 
-hamburger.addEventListener('click', () => {
-  console.log('Hi');
+myBtns.forEach((e) => {
+  e.addEventListener('click', () => {
+    dropdown.classList.toggle('visible-menu');
+    dropdown.classList.toggle('hidden');
+  });
 });
